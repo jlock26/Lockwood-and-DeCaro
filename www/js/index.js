750e712f0c4dbc10bbcccd3bebca61280eec9e75
ANT-206 <44479546+ANT-206@users.noreply.github.com>
2019-02-17 14:51:21 -0800

    storage stuff

--- www/js/index.js
+++ www/js/index.js
@@ -41,6 +41,31 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    document.getElementById("setLocalStorage").addEventListener("click", setLocalStorage); 
+    document.getElementById("showLocalStorage").addEventListener("click", showLocalStorage); 
+    document.getElementById("removeProjectFromLocalStorage").addEventListener 
+   ("click", removeProjectFromLocalStorage); 
+    document.getElementById("getLocalStorageByKey").addEventListener 
+   ("click", getLocalStorageByKey);  
+    var localStorage = window.localStorage;
+
+    function setLocalStorage() { 
+   localStorage.setItem("Name", "John"); 
+   localStorage.setItem("Job", "Developer"); 
+   localStorage.setItem("Project", "Cordova Project"); 
+} 
+
+    function showLocalStorage() { 
+   console.log(localStorage.getItem("Name")); 
+   console.log(localStorage.getItem("Job")); 
+   console.log(localStorage.getItem("Project")); 
+}   
+    function removeProjectFromLocalStorage() {
+   localStorage.removeItem("Project");
+}
+    function getLocalStorageByKey() {
+   console.log(localStorage.key(0));
+}
 };
 
 app.initialize();
