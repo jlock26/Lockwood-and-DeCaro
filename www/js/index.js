66eb7d1b36e6096ab4ec10271684e3aefccc435c
Jonathan Lockwood <jonathan@Jonathans-MacBook-Pro.local>
2019-03-12 17:30:56 -0700

    random

--- www/js/index.js
+++ www/js/index.js
@@ -43,32 +43,46 @@ var app = {
     }
 };
 
-    document.getElementById("setLocalStorage").addEventListener("click", setLocalStorage); 
-    document.getElementById("showLocalStorage").addEventListener("click", showLocalStorage); 
-    document.getElementById("removeProjectFromLocalStorage").addEventListener 
-      ("click", removeProjectFromLocalStorage); 
-    document.getElementById("getLocalStorageByKey").addEventListener 
-      ("click", getLocalStorageByKey);  
-    var localStorage = window.localStorage;
+document.getElementById("setLocalStorage").addEventListener("click", setLocalStorage); 
+document.getElementById("showLocalStorage").addEventListener("click", showLocalStorage); 
+document.getElementById("removeProjectFromLocalStorage").addEventListener 
+  ("click", removeProjectFromLocalStorage); 
+document.getElementById("getLocalStorageByKey").addEventListener 
+  ("click", getLocalStorageByKey);  
+var localStorage = window.localStorage;
 
-    function setLocalStorage() { 
-      localStorage.setItem("Name", "John"); 
-      localStorage.setItem("Job", "Developer"); 
-      localStorage.setItem("Project", "Cordova Project"); 
-    } 
+function setLocalStorage() { 
+  localStorage.setItem("Name", "John"); 
+  localStorage.setItem("Job", "Developer"); 
+  localStorage.setItem("Project", "Cordova Project"); 
+} 
 
-    function showLocalStorage() { 
-      console.log(localStorage.getItem("Name")); 
-      console.log(localStorage.getItem("Job")); 
-      console.log(localStorage.getItem("Project")); 
-    } 
+function showLocalStorage() { 
+  console.log(localStorage.getItem("Name")); 
+  console.log(localStorage.getItem("Job")); 
+  console.log(localStorage.getItem("Project")); 
+} 
 
-    function removeProjectFromLocalStorage() {
-      localStorage.removeItem("Project");
-    }
+function removeProjectFromLocalStorage() {
+  localStorage.removeItem("Project");
+}
 
-    function getLocalStorageByKey() {
-      console.log(localStorage.key(0));
-    }
+function getLocalStorageByKey() {
+  console.log(localStorage.key(0));
+}
+
+document.addEventListener("volumeupbutton", callbackFunction, false);  
+
+function callbackFunction() { 
+  alert('Volume Up Button is pressed!');
+}
+
+document.addEventListener("backbutton", onBackKeyDown, false);  
+
+function onBackKeyDown(e) { 
+   e.preventDefault(); 
+   alert('Back Button is Pressed!'); 
+}
+
 
 app.initialize();
